2286bf3eb9ff0073ed8a194f694121a668e616a3
Andrew Louis <andrew@plutocard.io>
2024-12-12 15:54:33 -0500

    gitignore and clean up

--- pp-scrape/dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JOB_STATUSES = void 0;
-exports.JOB_STATUSES = [
-    "pending",
-    "processing",
-    "completed",
-    "failed",
-];
